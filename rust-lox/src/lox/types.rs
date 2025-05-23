2764a404a6b683d9004686b00eaeb62c1332e6dc
Adowrath <adowrath@protonmail.ch>
2025-05-23 10:14:17 +0200

    Update common types
    Signed-off-by: Adowrath <adowrath@protonmail.ch>

--- rust-lox/src/lox/types.rs
+++ rust-lox/src/lox/types.rs
@@ -1,6 +1,7 @@
 //! Types used through multiple phases of the Lox project, mostly
 //! in an auxiliary fashion to support (better) error reporting.
 
+use core::cmp::{max, min, Ordering};
 use core::fmt::{Display, Formatter};
 
 /// A Location simply consists of a line and column position.
@@ -16,6 +17,17 @@ pub struct Location {
     pub col: usize,
 }
 
+impl PartialOrd for Location {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+impl Ord for Location {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.line.cmp(&other.line).then(self.col.cmp(&other.col))
+    }
+}
+
 impl Display for Location {
     fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
         write!(f, "{}:{}", self.line, self.col)
@@ -25,7 +37,7 @@ impl Display for Location {
 /// Source Spans define locations of elements in the source code,
 /// given both their start and end positions as [`Locations`](Location).
 /// Both positions are to be treated as inclusive.
-#[derive(Debug, PartialEq)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 #[non_exhaustive] // Filename might be added.
 pub struct Span {
     /// Start of the Span
@@ -37,8 +49,33 @@ pub struct Span {
 impl Span {
     /// Construct a source span from given start and end positions
     #[must_use]
-    pub fn from(start: Location, end: Location) -> Self {
+    pub const fn from(start: Location, end: Location) -> Self {
         Span { start, end }
+    }
+
+    /// Merges the given iterable of Spans. The iterable must NOT be empty!
+    ///
+    /// # Panics
+    ///
+    /// If the given iterable was empty (so that we do not need to represent
+    /// invalid spans).
+    /// TODO: Maybe implement invalid spans.
+    #[must_use]
+    pub fn merge<'a, I: IntoIterator<Item = &'a Span>>(spans: I) -> Span {
+        let mut spans = spans.into_iter();
+
+        #[expect(clippy::panic, reason = "explicitly warned in docs")]
+        let Some(first) = spans.next() else {
+            panic!("Span::merge was called with empty iterator.");
+        };
+
+        spans.fold(*first,
+            |mut acc, next| {
+                acc.start = min(acc.start, next.start);
+                acc.end = max(acc.end, next.end);
+                acc
+            }
+        )
     }
 }
 
@@ -53,13 +90,13 @@ impl Display for Span {
 }
 
 /// A Located value pairs a value with a source span.
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 #[expect(clippy::exhaustive_structs, reason = "this is set in stone")]
 pub struct Located<T>(pub T, pub Span);
 
 /// Raw Literals inside the Lox Language, they carry with their value
 /// the raw source code that was used to declare them.
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 #[expect(
     clippy::exhaustive_enums,
     reason = "adding a new variant MUST be handled and is a breaking change."
@@ -95,6 +132,6 @@ impl RawLiteral {
 }
 
 /// Identifiers inside the Lox Language.
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 #[expect(clippy::exhaustive_structs, reason = "Identifiers stay as Strings.")]
 pub struct Identifier(pub String);
